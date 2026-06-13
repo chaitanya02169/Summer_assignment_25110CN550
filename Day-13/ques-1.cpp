27aef3dceab20a57b9b10bfe5e2fda3cc0dadf0c
chaitanya santosh patil <chaitanya.patil21112007@gmail.com>
2026-06-14 00:01:25 +0530

    summer assignment Day13

---
+++ Day-13/ques-1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+int main () {
+    int a[5] ;
+    cout << " Enter 5 elements : ";
+    for(int i = 0 ; i < 5 ; i++ ) {
+        cin >> a[i] ;
+    }
+    cout << " Array elements are : " ;
+    for ( int i = 0 ; i < 5 ; i++ ) {
+        cout <<  a[i] << "  " ; 
+    }
+    return 0 ;
+}
