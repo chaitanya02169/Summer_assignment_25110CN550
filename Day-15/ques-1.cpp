55e2329bc264773e20b71953574b8a1f3f7c3c40
chaitanya santosh patil <chaitanya.patil21112007@gmail.com>
2026-06-15 23:42:20 +0530

    Summer assignment Day15

---
+++ Day-15/ques-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100], n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the array elements:\n";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Reversed array : " ;
+    for (int i = n - 1 ; i >= 0 ; i--){
+        cout << arr[i] << " " ;
+    }
+    return 0 ;
+}
